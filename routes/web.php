437bd06bedb548f34dc1e124406bc9c8535a9700
Amitav Roy <reachme@amitavroy.com>
2018-03-11 11:17:27 +0530

    Removed the unwanted code from routes file.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-use App\User;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\ActivateUserAccount;
-
-Route::get('mail', function() {
-    $user = User::find(2);
-
-    // Mail::to($user)->send(new ActivateUserAccount($user));
-    return new ActivateUserAccount($user);
-});
-
 Route::get('/', 'GuestController@welcome');
 Route::get('/activate-account/{token}', 'GuestController@verify')->name('activate-account');
 
